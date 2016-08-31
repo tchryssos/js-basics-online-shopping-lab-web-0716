a119f5c2631072f8d7f8b91d6717e670adf8fc38
Troy Chryssos <github email address>
2016-08-30 20:00:13 -0400

    commiting for alice, up to removing items from cart

--- index.js
+++ index.js
@@ -1,4 +1,39 @@
-var cart;
+var cart=[];
+
+function getCart(){
+  return cart;
+}
+
+function addToCart(item){
+  cart.push({[item]: (Math.floor(Math.random()*(101)+0))});
+  console.log(`${item} has been added to your cart.`);
+  return cart;
+}
+
+function viewCart(){
+  if (cart.length === 0) {
+    console.log("Your shopping cart is empty.");
+  } else {
+    var cartText = "In your cart, you have";
+    for(var i=0; i<cart.length; i++){
+      var item = cart[i];
+      for (var itemName in item) {
+        cartText += ` ${itemName} at $${item[itemName]},`;
+      }
+    }
+    console.log(cartText.slice(0, -1) + ".");
+  }
+}
+
+function removeFromCart(name) {
+  for(var i=0; i<cart.length; i++){
+    var item = cart[i];
+    for (var itemName in item) {
+      if (name === itemName) {
+        
+      }
+    }
+}
 
 function setCart(newCart) {
   cart = newCart;
